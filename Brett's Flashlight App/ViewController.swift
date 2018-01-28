ec0c5e0694b3a7b3edb586bb40913e97e94c6bba
Brett Bertola <Brett.bertola@gmail.com>
2018-01-27 21:57:12 -0700

    Initial Commit

---
+++ Brett's Flashlight App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Brett's Flashlight App
+//
+//  Created by Brett Bertola on 1/27/18.
+//  Copyright © 2018 Brett Bertola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
